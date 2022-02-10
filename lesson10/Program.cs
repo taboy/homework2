c668971ee0b5eef6b17b0b2c24f34fda9698641a
Taleh Mirzayev <40539143+taboy@users.noreply.github.com>
2022-02-10 11:00:17 +0300

    Finish

--- lesson10/Program.cs
+++ lesson10/Program.cs
@@ -1,10 +1,9 @@
 ﻿//Показать вторую цифру трёхзначного числа.
-  Console.WriteLine("write number");
-           int num = Convert.ToInt16(Console.ReadLine());
-           void vtoroecislo (int num){
-           int	num1=num !/ 10;
-           int num2=num1 % 10;
-           	Console.WriteLine(num2);
-           }
-        vtoroecislo(num);
-        }
+           Console.WriteLine("write number"); 
+           int num = Convert.ToInt16(Console.ReadLine()); 
+           void vtoroecislo (int num){ 
+           int num1=num !/ 10; 
+           int num2=num1 % 10; 
+            Console.WriteLine(num2); 
+           } 
+           vtoroecislo(num);
