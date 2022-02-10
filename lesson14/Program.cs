55fb3cb77df1bf68801652f062e735aef047cf2a
Taleh Mirzayev <40539143+taboy@users.noreply.github.com>
2022-02-10 11:38:09 +0300

    Update Program.cs

--- lesson14/Program.cs
+++ lesson14/Program.cs
@@ -1,1 +1,11 @@
 ﻿// Задача 14: Найти третью цифру числа или сообщить, что её нет.
+Random rnd = new Random(); 
+int a = rnd.Next(1, 999); 
+Console.WriteLine(a); 
+string b = Convert.ToString(a); 
+if (b.Length > 2) 
+{ 
+    Console.WriteLine(b[2]); 
+} 
+else  
+Console.WriteLine("No third digit ");
